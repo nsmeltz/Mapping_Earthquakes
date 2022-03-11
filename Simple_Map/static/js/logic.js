c929923f3de0fe184e9159685baa0f90d330929b
nsmeltz <smeltznatalie@gmail.com>
2022-03-11 13:42:16 -0600

    organizing

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -3,7 +3,7 @@ console.log("working");
 
 // ---------------- Add a Map Object -------------------
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([40.7, -94.5], 5);
 
 // OR
 
