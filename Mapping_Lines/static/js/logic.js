f9a4b1a3edd44b4274cbf73135360e1d4413f61b
nsmeltz <smeltznatalie@gmail.com>
2022-03-11 15:00:13 -0600

    mapping lines

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,26 @@
+// ---------------- Add a Map Object -------------------
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([[36.1733, -120.1794]], 7);
+
+
+// ----------------- Add Tile Layer --------------------
+// Create & add basemap tile layer
+let basemap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+}).addTo(map);
+
+// ---------------- Add Lines to Map --------------------------------
+
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790]
+];
+
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "red"
+}).addTo(map);
