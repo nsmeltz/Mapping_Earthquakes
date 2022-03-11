533ab9bf75e2001972921e06a436d207f159f8b6
nsmeltz <smeltznatalie@gmail.com>
2022-03-11 14:08:58 -0600

    circles & markers

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -3,20 +3,12 @@ console.log("working");
 
 // ---------------- Add a Map Object -------------------
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 5);
-
-// OR
-
-// // Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [40.7, -94.5],
-//     zoom: 4
-// });
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 
 // ----------------- Add Tile Layer --------------------
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -24,3 +16,22 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// ------------- Add Marker to Map ------------------
+//  Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// ------------ Add Circle to Map ---------------
+// Laramie, WY
+L.circle([41.3114, -105.5911], {
+    radius: 10000,
+    color: "black",
+    fillColor: "pink"
+}).addTo(map);
+
+// Los Angeles, CA
+ L.circleMarker([34.0522, -118.2437],{
+     radius: 300,
+     color: "black",
+     fillColor: "#ffffa1"
+ }).addTo(map);
